9694beb4bc04a5efbb9f07a9b706c0b0395a9319
AlexandreP77 <alexandredasilva.piedade@gmail.com>
2023-04-07 16:28:13 +0200

    test

--- tests/test_password_complexity.php
+++ tests/test_password_complexity.php
@@ -2,16 +2,14 @@
     include '../fonction.php';
     
     $test1 = create_user("username", "password");
-    $test2 = create_user("username", "@MotsdePasse?");
-    $test3 = create_user("username", "@MotsdePasse?");
-    $test4 = create_user("username", "motsdepasse1");
-    $test5 = create_user("username", "m0tdepasse");
-    $test6 = create_user("username", "@MotsdePasse");
-    
-    if ($test1 == 4 && $test2 == 0 && $test3 == 1 && $test4 == 2 && $test5 == 1 && $test6 == 0) {
+    $test2 = create_user("username", "P@ssw0rd!");
+    $test3 = create_user("username", "P@SSW0RD!");
+    $test4 = create_user("username", "password1");
+    $test5 = create_user("username", "Passw0rd");
+    $test6 = create_user("username", "P@ssword");
+
+    if ($test1 == 2 && $test2 < 2 && $test3 == 2 && $test4 == 2 && $test5 == 2 && $test6 == 2) {
         printf("true");
     } else {
         printf("false");
     }
-    ?>
-    
