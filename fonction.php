b295fe1182a5fa809ee560f680e89e1397ec9b5c
AlexandreP77 <alexandredasilva.piedade@gmail.com>
2023-04-07 14:57:27 +0200

    Update fonction.php

--- fonction.php
+++ fonction.php
@@ -1,105 +1,135 @@
 <?php
-    function create_user($login, $password) {
-        if (!ctype_alnum($login) || !preg_match('/^[a-zA-Z0-9]+$/', $login)) {
-            return 3; // invalid username
-        }
-
-        if (!verifyPassword($password)) {
-            return 2; // invalid password
-        }
-
+    function create_user($login, $password)
+    {
         $filePath = __DIR__ . "/users/" . $login . ".txt";
         $filePath = str_replace("\\", "/", $filePath);
 
-        if (file_exists($filePath)) {
-            return 1; // user already exists
+        if (!empty($login) && !empty($password)) {
+            if (ctype_alnum($login) && preg_match('/^[a-zA-Z0-9]+$/', $login)) {
+                if (verifyPassword($password)) {
+                    if (!file_exists($filePath)) {                                  
+                        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+                        file_put_contents($filePath, $hashed_password);
+                        return 0;
+                    } else {
+                        return 1;
+                    }
+                }else{
+                    return 2;
+                }
+            }else{
+                return 3;
+            }
+        }else{
+            return 4;
         }
-
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        file_put_contents($filePath, $hashed_password);
-        return 0; // success
     }
 
     function connect_user($login, $password) {
-        if (!ctype_alnum($login) || !preg_match('/^[a-zA-Z0-9]+$/', $login)) {
-            return 3; // invalid username
-        }
-
         $filePath = __DIR__ . "/users/" . $login . ".txt";
         $filePath = str_replace("\\", "/", $filePath);
-
-        if (!file_exists($filePath)) {
-            return 2; // user does not exist
+    
+        if (!empty($login) && !empty($password)) {
+            if (ctype_alnum($login) && preg_match('/^[a-zA-Z0-9]+$/', $login)) {
+                if (file_exists($filePath)) {
+                    $stored_password = file_get_contents($filePath);
+                    if (password_verify($password, $stored_password)) {
+                        $_SESSION["CONNECTED"] = $login;
+                        return 0; 
+                    } else {
+                        return 1; 
+                    }
+                } else {
+                    return 2; 
+                }
+            } else {
+                return 3; 
+            }
+        } else {
+            return 4;
         }
-
-        $stored_password = file_get_contents($filePath);
-
-        if (!password_verify($password, $stored_password)) {
-            return 1; // invalid password
-        }
-
-        $_SESSION["CONNECTED"] = $login;
-        return 0; // success
     }
         
     function disconnect_user($username) {
         if (isset($_SESSION["CONNECTED"]) && $_SESSION["CONNECTED"] == $username) {
             unset($_SESSION["CONNECTED"]);
-            return 0; // success
+            return 0;
         } else {
-            return 1; // user not connected
+            return 1;
         }
     }
 
     function update_user($username, $old_password, $new_password) {
-        if (!ctype_alnum($username) || !preg_match('/^[a-zA-Z0-9]+$/', $username)) {
-            return 4; // invalid username
-        }
-
-        if (!verifyPassword($new_password)) {
-            return 1; // invalid new password
-        }
-
         $filePath = __DIR__ . "/users/" . $username . ".txt";
         $filePath = str_replace("\\", "/", $filePath);
+    
+        if (!empty($username) && !empty($old_password) && !empty($new_password)) {
+            if (ctype_alnum($username) && preg_match('/^[a-zA-Z0-9]+$/', $username)) {
+                if (file_exists($filePath)) {
+                    $stored_password = file_get_contents($filePath);
+                    if (password_verify($old_password, $stored_password)) {
+                        if (verifyPassword($new_password)) {
+                            $hashed_new_password = password_hash($new_password, PASSWORD_DEFAULT);
+                            file_put_contents($filePath, $hashed_new_password);
 
-        if (!file_exists($filePath)) {
-            return 3; // user does not exist
-        }
-
-        $stored_password = file_get_contents($filePath);
-
-        if (!password_verify($old_password, $stored_password)) {
-            return 2; // invalid password
-        }
-
-        $hashed_new_password = password_hash($new_password, PASSWORD_DEFAULT);
-        file_put_contents($filePath, $hashed_new_password);
-        return 0; // success
+                            return 0;
+                        } else {
+                            return 1; 
+                        }
+                    } else {
+                        return 2; 
+                    }
+                } else {
+                    return 3; 
+                }
+            } else {
+                return 4;
+            }
+        } else {
+            return 5; 
     }
     
     function delete_user($username) {
-        if (!ctype_alnum($username) || !preg_match('/^[a-zA-Z0-9]+$/', $username)) {
-            return 2; // invalid username
-        }
-
         $filePath = __DIR__ . "/users/" . $username . ".txt";
         $filePath = str_replace("\\", "/", $filePath);
-
-        if (!file_exists($filePath)) {
-            return 1; // user does not exist
+    
+        if (!empty($username)) {
+            if (ctype_alnum($username) && preg_match('/^[a-zA-Z0-9]+$/', $username)) {
+                if (file_exists($filePath)) {
+                    unlink($filePath);
+                    return 0; 
+                } else {
+                    return 1;
+                }
+            } else {
+                return 2; 
+            }
+        } else {
+            return 3; 
         }
-
-        unlink($filePath);
-        return 0; // success
     }
     
     function verifyPassword($password) {
         if (strlen($password) < 8) {
             return false;
         }
-
         if (!preg_match('/[A-Z]/', $password)) {
             return false;
         }
-    }
+        if (!preg_match('/[a-z]/', $password)) {
+            return false;
+        }
+        if (!preg_match('/[0-9]/', $password)) {
+            return false;
+        }
+        $allowed_special_chars = '!@#$%^&*()-_=+.,:;>';
+        $pattern = '/[' . preg_quote($allowed_special_chars, '/') . ']/';
+        if (!preg_match($pattern, $password)) {
+            return false;
+        }
+        if (preg_match('/[{}()\[\]|\\_\/<]/', $password)) {
+            return false;
+        }
+        return true;
+    } 
+}
